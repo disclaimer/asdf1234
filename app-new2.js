b69b5acb734c4ae171399ab82ed58211101e170c
disclaimer <disclaimer@dormiu.com>
2020-02-04 18:47:59 -0300

    Update app-new2.js

--- app-new2.js
+++ app-new2.js
@@ -241,10 +241,16 @@ var AlarmList = React.createClass({
             }
         }.bind(this);
 
-        return React.createElement(
-            'ul',
-            { className: 'alarmList list-group' },
-            list()
+        return React.createElement('div', { className: 'table-responsive' },
+            React.createElement('table', { className: 'table table-condensed' },
+                React.createElement('tr', { className: 'someclass' },
+                    React.createElement('td', { className: 'someclass' }, 'Active'),
+                    React.createElement('td', { className: 'someclass' }, 'Time'),
+                    React.createElement('td', { className: 'someclass' }, 'Description'),
+                    React.createElement('td', { className: 'someclass' }, 'Delete')
+                ),
+                list()
+            )
         );
     }
 });
