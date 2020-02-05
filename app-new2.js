2496ac3200f7a9e1d278f39025f8190e54ba8684
disclaimer <disclaimer@dormiu.com>
2020-02-05 17:13:13 -0300

    Update app-new2.js

--- app-new2.js
+++ app-new2.js
@@ -428,6 +428,16 @@ var Alarm = React.createClass({
                         ),
                         React.createElement(AlarmList, { data: data, ref: 'alarmList', onRing: this.handleRing })
                     )
+                ),
+                React.createElement('div', { className: 'panel panel-default' },
+                    React.createElement('div', { className: 'panel-heading' },
+                        React.createElement('h3', { className: 'panel-title' },
+                          React.createElement('span', null, 'Alarm clock settings')
+                        )
+                    ),
+                    React.createElement('div', { className: 'panel-body' },
+                        React.createElement(Bell, { ref: 'bell', bells: this.state.bells, onAddAudio: this.handleAddAudio })
+                    )
                 )
             )
         );
